d19ef7442de7f406eff90d61e6dc7b468a27f31a
Liam Cox <liamcox@Liams-MacBook-Pro.local>
2020-11-23 16:01:23 -0600

    added pointer to map

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,8 +1,5 @@
-mapboxgl.accessToken = mapToken;
+const campground = require("../../models/campground");
 
-const map = new mapboxgl.Map({
-    container: "map",
-    style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 4, // starting zoom
-});
+
+
+
