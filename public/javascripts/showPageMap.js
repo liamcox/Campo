b2b8c1140fd21787603b561050d8d581d269fccc
Liam Cox <liamcox@Liams-MacBook-Pro.local>
2020-11-23 20:36:35 -0600

    fixed map bug

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,5 +1,12 @@
-const campground = require("../../models/campground");
+mapboxgl.accessToken = mapToken;
+console.log(mapToken);
+const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+    center: campground.geometry.coordinates, // starting position [lng, lat]
+    zoom: 10, // starting zoom
+});
 
-
-
-
+const marker = new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .addTo(map);
